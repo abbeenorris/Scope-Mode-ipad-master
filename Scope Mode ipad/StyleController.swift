eb95bce8a665f9ec1a717185cce9964d27a9c17f
kmcgowan0 <i7673473@Mac27779.local>
2016-05-12 15:57:01 +0100

    hi sam it works now

--- Scope Mode ipad/StyleController.swift
+++ Scope Mode ipad/StyleController.swift
@@ -1,5 +1,8 @@
 // Hi kt, the code on this page doens't work, it wont pass the data in hiddenProjectVar(the data comes from ProjectNameConttroller and is passed through all the controllers up until this point) the problem is that we have 3 buttons on the page all going to different view controllers so i put the code to pass the data to the correct view controller into a action button which is linked to the right segue but this doesn't work. Could you ask Kyle about this? thanks Sam.
 
+
+//hi sam it works now
+
 import Foundation
 import UIKit
 
@@ -22,12 +25,12 @@ class StyleController: UIViewController {
             destViewController.hiddenProjectVar = hiddenProjectVar
         }
         
-        if segue.identifier == "LiveAction" {
+        if segue.identifier == "Animation" {
             let destViewController = segue.destinationViewController as! AnimationController
             destViewController.hiddenProjectVar = hiddenProjectVar
         }
         
-        if segue.identifier == "LiveAction" {
+        if segue.identifier == "MixedMedia" {
             let destViewController = segue.destinationViewController as! MixedMediaController
             destViewController.hiddenProjectVar = hiddenProjectVar
         }
